97223470fdbf9a4811881000a817136547872225
gedrix <gerardo.rm93@hotmail.com>
2020-08-12 21:52:27 -0500

    pruebas de las funciones contador de linea y tiempoProyecto25

--- proyecto_novenociclo/Test/ProcesoTest.php
+++ proyecto_novenociclo/Test/ProcesoTest.php
@@ -11,6 +11,8 @@ class ProcesoTest extends TestCase
     //    $this->assertEquals($animal->esfuerzoAplicado(), 'Muuu');
     // }
 
+    /***** PRUEBAS GERARDO RAMIREZ    *****/
+    /***** FUNCION ESFUERZOAPLICAO    *****/
 	//funcion esfuerzoAplicado, cuando todos los valores son nulos
     public function testEsfuerzoAplicadoVacio() {
         
@@ -41,7 +43,127 @@ class ProcesoTest extends TestCase
         $valor_redondeado = round ($operacion_multiplicacion, 2);
         $this->assertEquals($proceso->esfuerzoAplicado($eaf,$valor_a, $valor_b, $linea_codigo), $valor_redondeado);
     }
+     //Cuando los valores que se envían a la función EsfuerzoAplicado son valores int negativos.
+    public function testEsfuerzoAplicadoVN()
+    {
+         $proceso = new proceso();
+        $eaf = 0.94;
+        $valor_a = 3;
+        $valor_b =  -1.12;
+        $linea_codigo = 20000;
+       
+        $this->assertEquals($proceso->esfuerzoAplicado($eaf,$valor_a, $valor_b, $linea_codigo), 'no se puede resolver');
+    } 
 
+    /***** FUNCION CONTADORLINEA    ****/
+    //funcion testContadorLinea, cuando todos los valores son vacios
+    public function testContadorLineaVacio() {
+        
+        $proceso = new proceso();
+
+        $this->assertEquals($proceso->contadorLinea("",""), 'no se puede resolver');
+    }
+
+    //funcion testContadorLinea, cuando todos los valores son correctos
+    public function testContadorLineaVS() {
+        
+        $elemento ='prueba.php';
+        $directorio='C:\Users\Gedo\Documents\ejerpython\pruebates';
+
+            $cont =0;
+            $coment =0;
+          $comm1 = "/*";
+            $comm2 = "*/";
+            $comTotal = 0;
+            $lineasC=0;
+            $banC = false;
+            
+            $archivo = fopen('C:\Users\Gedo\Documents\ejerpython\pruebates/prueba.php',"r");
+            while(!feof($archivo))
+            {
+
+                $traer = fgets($archivo);// Leyendo una linea 
+
+                if (!ctype_space($traer)) //ctype_space Chequear posibles caracteres de espacio en blanco
+                {
+                    $trimmed = trim($traer); //elimino espacios en blanco al inicio y final de la cadena
+                    if (substr($trimmed, 0,1) != "") {
+                        $posicion_cadena = substr($trimmed, 0,2); //con esta cadena voy a tomar en cuenta las 2 primeras posicions para omitir el //
+                        $posicion_cadena2 = substr($trimmed, 0,1); //con esta cadena voy a omitir el #
+                        $tzt = strstr($trimmed, $comm1); //encuentra /*
+                        $txt = strstr($trimmed, $comm2); //encuentra */
+                            if ($tzt != ""){
+                                
+                                $banC = true;
+                            }else if ($txt != ""){
+                                $coment++;
+                                $comTotal += $coment + $lineasC;
+                                $coment=0;
+                                $lineasC=0;
+                                $banC = false;
+                            }
+                            if ($posicion_cadena != "//" ) {
+                                if ($posicion_cadena2 != "#") {
+                                    $cont++;
+                                    if($banC == true){
+                                        $lineasC += 1;
+                                    }
+                                }
+                            }
+                    }
+                    
+                }
+            }
+            fclose($archivo); // Cerrando el archivo
+            $totalR = $cont - $comTotal;
+
+        $proceso = new proceso();
+
+        $this->assertEquals($proceso->contadorLinea($elemento, $directorio), $totalR);
+    }
+
+    //funcion tiempoProyecto25, cuando todos los valores son nulos
+    public function testtiempoProyecto25Vacio() {
+        
+        $proceso = new proceso();
+
+        $this->assertEquals($proceso->tiempoProyecto25("","",""), 'no se puede resolver');
+    } 
+
+    //funcion tiempoProyecto25, cuando todos los valores string
+    public function testtiempoProyecto25VS() {
+        
+        $proceso = new proceso();
+
+        $this->assertEquals($proceso->tiempoProyecto25("a","b","c"), 'no se puede resolver');
+    } 
+
+    //funcion tiempoProyecto25, cuando  todos los valores son int
+    public function testtiempoProyecto25VI() {
+
+        $proceso = new proceso();
+        
+        $valor_c = 2.5;
+        $total_esfuerzo =  1.12;
+        $valor_d = 0.38;
+        $operacion_potencia = pow($total_esfuerzo, $valor_d );
+        $operacion_multiplicacion = $valor_c * $operacion_potencia;
+        $tiempo_olgura = $operacion_multiplicacion * 0.25;
+        $tiempo_total = $operacion_multiplicacion + $tiempo_olgura;
+        $totalTest = round($tiempo_total, 1);
+        $this->assertEquals($proceso->tiempoProyecto25($valor_c,$total_esfuerzo, $valor_d), $totalTest);
+    } 
+
+    //Cuando los valores que se envían a la función tiempoProyecto25 son valores int negativos.
+    public function testtiempoProyecto25VN()
+    {
+         $proceso = new proceso();
+         $valor_c = 2.5;
+         $total_esfuerzo =  -1.12;
+         $valor_d = 0.38;
+       
+        $this->assertEquals($proceso->tiempoProyecto25($valor_c,$total_esfuerzo, $valor_d), 'no se puede resolver');
+    } 
     /*--------------- RICARDO ESPARZA ----------------*/
     //funcion leerArchivos, cuando el valor de la ruta es vacio
     public function testLeerArchivosVacio(){
