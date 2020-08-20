7c741f5af9c342ef2bf97dec0d67d02b19782c63
SamGonzalez28 <samantha.gonzalez@unl.edu.ec>
2020-08-20 00:01:47 -0500

    Comentarios en el archivo reporte.php
    Comentarios de codigo del archivo

--- proyecto_novenociclo/reporte.php
+++ proyecto_novenociclo/reporte.php
@@ -1,5 +1,15 @@
 <?php ob_start();
- 
+    
+    /*
+        BLOQUE DE CÓDIGO PHP
+
+        Al inicializar esta pagina, se recibe una variable v1 mediante el método get, que recibe una cadena con los resultados calculados
+        en cargar archivo.php y se los divide en un array para ser presentados
+        
+        $cad = almacena la cadena de variables
+        $valores = almacena el array de con los valores recibidos
+
+     */
     $cad=$_GET['v1'];
     $valores = explode("-", $cad);
 ?>
@@ -7,6 +17,10 @@
 <head>
     <title></title>
 </head>
+<!--
+        Bloque de codigo HTML5 para crear la visualización de las variables tratadas en el bloque de codigo php, 
+        que serán impresas en el reporte 
+ -->
 <body>
 <center><img src="cocomo.png" width="100%"></center>
 <h2>Reporte de Estimación</h2>
@@ -72,13 +86,20 @@
 </html>
 
 <?php
-require_once 'dompdf/autoload.inc.php';
-use Dompdf\Dompdf;
-$dompdf = new DOMPDF();
-$dompdf->load_html(ob_get_clean());
-$dompdf->render();
-$pdf = $dompdf->output();
-$filename = "reporte.pdf";
+/*
+        BLOQUE DE CÓDIGO PHP
+
+        Al finalizar el tratamiento de variables, descrito en el bloque de html5 se procede a utilizar la herramienta DomPdf para generar el
+        pdf del reporte.
+        
+*/
+require_once 'dompdf/autoload.inc.php';//requiere el archivo autoload, que pertenece a la herramienta DomPdf
+use Dompdf\Dompdf;//utiliza la herramienta
+$dompdf = new DOMPDF();//crea un objeto de la clase
+$dompdf->load_html(ob_get_clean());//carga la vista html5
+$dompdf->render();//procesa la vista
+$pdf = $dompdf->output();//guarda la salida
+$filename = "reporte.pdf";//se asigna un nombre por defecto al archivo pdf
 file_put_contents($filename, $pdf);
-$dompdf->stream($filename);
+$dompdf->stream($filename);//envia a descargar el archivo con el nombre asignado
 ?>
